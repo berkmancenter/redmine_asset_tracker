dc1d1e36c37e90183c175ec5a6a9dbd067553d19
Nitish Upreti <nitishupreti@gmail.com>
2012-06-24 23:34:00 +0530

    Fixing the model reload issue.

--- lib/patches/user_patch.rb
+++ lib/patches/user_patch.rb
@@ -8,13 +8,9 @@ module UserPatch
 
     base.send(:include, InstanceMethods)
 
-    # Same as typing in the class 
-    base.class_eval do
-      #unloadable # Send unloadable so it will not be unloaded in development
-      has_many :favourites, :dependent => :destroy
-      
-    end
+    base.unloadable 
 
+    base.has_many :favourites, :dependent => :destroy
   end
   
   module ClassMethods
