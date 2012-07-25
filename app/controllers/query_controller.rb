3488b065211a2f502e1e409ee10f4749abfece32
Nitish Upreti <nitishupreti@gmail.com>
2012-07-25 16:31:11 +0530

    Updated Search for Assets based on Custom Attributes.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -17,6 +17,7 @@ class QueryController < ApplicationController
 
     conditions = []
     value = {}
+    result = []
     @result_assets = []
     @result_groups = []
 
@@ -38,12 +39,56 @@ class QueryController < ApplicationController
     conditions = conditions.join(" AND ")
 
     if params[:nature] == 'Asset' then
-        @result_assets = Asset.all(:conditions => [conditions,value])
+        #Find results for all custom attributes
+        list_of_custom_attributes_id = Array.new
+        keylist = params.keys
+
+        keylist.each do |k|
+          list_of_custom_attributes_id.push(k) if k.is_number? 
+        end
+
+        #Check for custom_attributes, this way gets a little ineffecient if we need to scale for thousands of assets
+        Asset.all.each do |asset|
+          
+          is_result = true
+
+          logger.info(asset.name)
+
+          list_of_custom_attributes_id.each do |l|
+
+            if asset.custom_value_for(l) == nil then
+              is_result = false
+              break
+            end
+
+            #if number compare directly
+            if asset.custom_value_for(l).value.is_number? then
+                unless asset.custom_value_for(l).value == params[l.to_sym]
+                  is_result = false
+                  break
+                end
+            else
+                unless /[[:alnum:]]*#{params[l.to_sym].downcase}[[:alnum:]]*/.match(asset.custom_value_for(l).value.downcase) 
+                  is_result = false
+                  break
+                end
+            end
+
+          end
+
+          result.push(asset) if is_result
+        end
+
+        #check for fixed attributes
+        result.each do |r|
+          if (params[:name].blank? ||  /[[:alnum:]]*#{params[:name].downcase}[[:alnum:]]/.match(r.name.downcase) ) && (params[:description].blank? ||  /[[:alnum:]]*#{params[:description].downcase}[[:alnum:]]/.match(r.description.downcase) ) && (params[:asset_type].blank? ||  /[[:alnum:]]*#{params[:asset_type].downcase}[[:alnum:]]/.match(r.asset_type.name.downcase) ) then
+            @result_assets.push(r)
+          end
+        end
+        logger.info(@result_assets)
     else
          #Get results from Asset Table for normal parameters
         @result_groups = AssetGroup.all(:conditions => [conditions, value])
     end
-
   end 
-
 end
